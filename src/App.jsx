aba0a641e8f1e2e6b6ebae08ff972744ef03fb88
lys163 <leeyongsuk20@naver.com>
2025-11-25 14:15:05 +0900

    aaa #1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="p-6 bg-black text-white text-3xl font-bold">
+      Tailwind v4 Working!
+    </div>
+  );
+}
